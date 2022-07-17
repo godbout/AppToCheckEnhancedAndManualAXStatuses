bf7b55f11b3e6f5c935a7d84fce66def094cde5f
Guillaume Leclerc <guill.bout@gmail.com>
2022-07-17 23:47:36 +0800

    Initial Commit

---
+++ AppToCheckEnhancedAndManualAXStatuses/AppToCheckEnhancedAndManualAXStatusesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppToCheckEnhancedAndManualAXStatusesApp.swift
+//  AppToCheckEnhancedAndManualAXStatuses
+//
+//  Created by Guillaume Leclerc on 17/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppToCheckEnhancedAndManualAXStatusesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
